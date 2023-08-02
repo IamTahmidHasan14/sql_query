d3434451e35de06922c307dd86a3100bb2201810
Tahmid Hasan <67729687+IamTahmidHasan14@users.noreply.github.com>
2023-08-02 07:05:34 +0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,11 @@
+create table students(
+ID int,
+Name varchar(15),
+CGPA float
+);
+
+insert into students values(67,'Tahmid',4)
+insert into students values(77,'Ahmad',3.5)
+insert into students values(87,'Khan',3.65)
+
+select *from students;
